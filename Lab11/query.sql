ce03e55f1b0a3278b2d58f182c0716e253e6f574
HamXa <hamza.wasym99@gmail.com>
2024-03-24 23:25:44 +0500

    lab11

---
+++ Lab11/query.sql
@@ -0,0 +1,4 @@
+/* stp_InsertProduct @ProductName = 'COCOMo',@UnitsInStock = '225', @UnitOnOrder='22',@QuantityPerUnit = '11',@UnitPrice = '120',@ReorderLevel
+ = '3',@Discontinued = 'False';*/
+
+select * from Products where ProductName = 'Cocomo'
